2512720690f0d90c0d8abaef676523fe087458cb
EC2 Default User <ec2-user@ip-172-31-40-154.ap-northeast-1.compute.internal>
2020-12-07 07:37:15 +0000

    remove test code

--- src/ClassDefault.tsx
+++ src/ClassDefault.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 class ClassDefault extends React.Component {
   render() {
-    return <h1>Default Export Class14</h1>;
+    return <h1>Default Export Class</h1>;
   }
 }
 
